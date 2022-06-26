bdd8c1a8cbcf70557b592c82468dce5ab68c0165
Richard Zampieri <rc_scott48@hotmail.com>
2022-06-26 14:45:34 -0700

    Updated create-project script added user input

--- Scripts/Python/create-proj.ps1
+++ Scripts/Python/create-proj.ps1
@@ -1,45 +1,79 @@
-﻿# Variable declarations
-$gitignore = "/Scripts/Python/Data/.gitignore"
+﻿<#
+.SYNOPSIS
+    PowerShell script to create python project template
+.DESCRIPTION
+	This script creates a basic python project structure including docs, examples, source, and test folders. Also contains a requirements.txt,
+    and a basic setup.py and setup.cfg
+.EXAMPLE
+	PS> ./create-proj
+	Project name: "your project name" <CONFIRM>
+	...
+.LINK
+	https://github.com/rsaz/PowerShell-Scripts/blob/main/Scripts/Python/create-proj.ps1
+.NOTES
+	Author: Richard Zampieri
+#>
 
-# Create a python project boilerplate
-Write-Output "Generating Python Project . . ."
 
-# Create Folder Structure
-New-Item project-template -ItemType Directory
-Set-Location project-template
-New-Item docs, examples, source, tests -ItemType Directory
-New-Item requirements.txt
+try {
+    # Variable declarations
+    $gitignore = "https://raw.githubusercontent.com/rsaz/PowerShell-Scripts/main/Scripts/Python/Data/.gitignore"
+    $projectName = Read-Host -Prompt 'Project name '
 
-# Setting up Main
-Set-Location source
-New-Item main.py, __init__.py
-New-Item package_name -ItemType Directory
-Set-Location package_name
-New-Item my_package.py, __init__.py
-Set-Location ..
+    # Create a python project boilerplate
+    Write-Output "=> Generating Python Project -----------------------------------"
 
-# Downloading gitgnore
-Write-Output "Downloading .gitgnore"
-Set-Location ..
-Invoke-WebRequest -Uri $gitignore -OutFile .\.gitignore
+    # Create Folder Structure
+    New-Item $projectName -ItemType Directory
+    Set-Location $projectName
+    New-Item docs, examples, source, tests -ItemType Directory
+    New-Item requirements.txt
 
-# Create a python environment
-Write-Output "Creating Python Enviornment . . ."
-py -m venv venv
+    # Setting up Main
+    Set-Location source
+    New-Item main.py, __init__.py
+    New-Item $projectName"_package" -ItemType Directory
+    Set-Location $projectName"_package"
+    New-Item $projectName".py", __init__.py
+    Set-Location ..\..\
+    Set-Location tests
+    New-Item $projectName"_package" -ItemType Directory
+    Set-Location $projectName"_package"
+    New-Item "test_${projectName}_package.py"
 
-# Activating python environment
-Write-Output "Activating Python Enviornment . . ."
-Set-Location .\venv\Scripts\
-.\activate
+    # Downloading gitgnore
+    Set-Location ..\..\
+    Invoke-WebRequest -Uri $gitignore -OutFile .\.gitignore
 
-# Installing basic dependencies
-Write-Output "Installing basic dependencies"
-Set-Location ..\..\
-py -m pip install autopep8 pytest
+    # Create a python environment
+    Write-Output "=> Creating Python Enviornment -----------------------------------"
+    py -m venv venv
 
-# Updating dependencies
-Write-Output "Updating project dependencies . . ."
-py -m pip freeze > requirements.txt
+    # Activating python environment
+    Write-Output "=> Activating Python Enviornment -----------------------------------"
+    Set-Location .\venv\Scripts\
+    .\activate
 
-# Finalizing the project creation
-Write-Output "Project created successfully"
+    # Updating Pip
+    Write-Output "=> Updating pip -----------------------------------"
+    Set-Location ..\..\
+    py -m pip install --upgrade pip
+
+    # Installing basic dependencies
+    Write-Output "=> Installing basic dependencies -----------------------------------"
+    py -m pip install autopep8 pytest
+
+    # Updating dependencies
+    Write-Output "=> Updating project dependencies -----------------------------------"
+    py -m pip freeze > requirements.txt
+
+    # Finalizing the project creation
+    Write-Output "=> Project created successfully -----------------------------------"    
+}
+catch {
+    Write-Host "An error occurred:"
+    Write-Host $_.ScriptStackTrace
+}
+
+
+
